66cbdcc6652fd211f0e97ddd4a8599b21e590895
bbangwon <bbangwon.chung@gmail.com>
2026-02-20 22:32:45 +0900

    불필요한 console.log 제거 및 패널 클릭 시 답변 토글 기능 추가

--- accordion/accordion_1.js
+++ accordion/accordion_1.js
@@ -1,10 +1,10 @@
 window.onload = () => {
   const panelFaqContainers = document.querySelectorAll(".panel-faq-container");
-  console.log(panelFaqContainers);
 
   for (let i = 0; i < panelFaqContainers.length; i++) {
     panelFaqContainers[i].addEventListener("click", function () {
-      console.log("나 클릭됨" + i);
+      const panelFaqAnswer = this.querySelector(".panel-faq-answer");
+      panelFaqAnswer.classList.toggle("active");
     });
   }
 };
