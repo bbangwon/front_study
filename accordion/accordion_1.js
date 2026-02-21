ce7f65b23e979d6234c4c960f0080f443133e4c9
bbangwon <bbangwon.chung@gmail.com>
2026-02-21 19:12:11 +0900

    패널 클릭 시 답변 토글 기능 개선 및 FAQ 총 합계 추가

--- accordion/accordion_1.js
+++ accordion/accordion_1.js
@@ -7,4 +7,12 @@ window.onload = () => {
       panelFaqAnswer.classList.toggle("active");
     });
   }
+
+  const btnAllClose = document.querySelector("#btn-all-close");
+  btnAllClose.addEventListener("click", function () {
+    const panelFaqAnswers = document.querySelectorAll(".panel-faq-answer");
+    for (let i = 0; i < panelFaqAnswers.length; i++) {
+      panelFaqAnswers[i].classList.remove("active");
+    }
+  });
 };
