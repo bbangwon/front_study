03f2dbe935b9ead1c8774693b49ee97241f0aa5c
bbangwon <bbangwon.chung@gmail.com>
2026-02-24 00:13:58 +0900

    localStorage 기능을 위한 입력 필드 수정 및 데이터 저장 기능 추가

---
+++ localstorage/localstorage.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+  const btnSetLocalData = document.querySelector(".btnSetLocalData");
+  const inputData = document.querySelector("input");
+
+  btnSetLocalData.addEventListener("click", () => {
+    localStorage.setItem("userid", inputData.value);
+    inputData.value = "";
+  });
+
+  const btnGetLocalData = document.querySelector(".btnGetLocalData");
+  btnGetLocalData.addEventListener("click", () => {
+    const userid = localStorage.getItem("userid");
+    if (userid) {
+      inputData.value = userid;
+    } else {
+      alert("저장된 데이터가 없습니다.");
+    }
+  });
+};
