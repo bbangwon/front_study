8bde1837e395dbd5b73be6d760d5554aa1be13db
youngil <youngil@claion.co.kr>
2026-03-04 00:34:18 +0900

    쿠키 삭제 기능 추가 및 쿠키 만료 기간 수정

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,8 +1,15 @@
 function setCookie() {
   let expiration = new Date();
-  expiration.setDate(expiration.getDate() + 10);
+  expiration.setDate(expiration.getDate() + 7);
   console.log(expiration);
   console.log(expiration.toUTCString());
+
+  let cookies = "";
+  cookies = "userid=superman;expires=" + expiration.toUTCString();
+  console.log(cookies);
+
+  document.cookie = cookies;
+  alert("쿠키를 생성하였습니다.");
 }
 
 function getCookie() {
@@ -13,4 +20,11 @@ function getCookie() {
   } else {
     alert(`저장된 쿠키의 값은: ${allCookies}\n(다시 방문해주셔서 환영합니다.)`);
   }
+}
+
+function delCookie() {
+  // document.cookie = "username=hongkildong";
+  // document.cookie = "username=leesoonsin"; //쿠키 수정
+  document.cookie = "username=;expires=Sat,01 Jan 1972 00:00:00";
+  alert("쿠키를 삭제하였습니다.");
 }
