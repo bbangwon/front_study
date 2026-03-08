eb73b3e32818e3f939bbcab7bf73dfa7da3a28d6
bbangwon <bbangwon.chung@gmail.com>
2026-03-08 12:37:12 +0900

    getCookie 함수 추가 및 setCookie 함수의 이벤트 리스너 수정

--- cookie_ex/cookie.js
+++ cookie_ex/cookie.js
@@ -1,4 +1,33 @@
-const setCookie = function () {
+const getCookie = function () {
+  const allCookies = document.cookie;
+  console.log(allCookies);
+
+  if (allCookies) {
+    alert("저장된 쿠키의 값은:" + allCookies);
+  } else {
+    alert("저장된 쿠키가 없습니다.");
+  }
+};
+
+const setCookie = function (cname, cvalue, cexpire) {
+  let expiration = new Date();
+  expiration.setDate(expiration.getDate() + parseInt(cexpire));
+
+  let cookies = "";
+  cookies += `${cname}=${cvalue};`;
+  cookies += `expires=${expiration.toGMTString()}`;
+  console.log(cookies);
+
+  document.cookie = cookies;
+};
+
+const delCookie = function (cname) {
+  setCookie(cname, "", 0);
+  alert("쿠키를 삭제했습니다.");
+};
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function () {
   event.preventDefault();
 
   cname = document.getElementById("cname").value;
@@ -10,21 +39,9 @@ const setCookie = function () {
   console.log("cvalue = " + cvalue);
   console.log("cexpire = " + cexpire);
 
-  let expiration = new Date();
-  expiration.setDate(expiration.getDate() + parseInt(cexpire));
-
-  let cookies = "";
-  cookies += `${cname}=${cvalue};`;
-  cookies += `expires=${expiration.toGMTString()}`;
-  console.log(cookies);
-
-  document.cookie = cookies;
+  setCookie(cname, cvalue, cexpire);
 
   document.getElementById("form").reset();
-
-  cname.focus();
+  document.getElementById("cname").focus();
   alert("쿠키를 생성했습니다.");
-};
-
-const form = document.getElementById("form");
-form.addEventListener("submit", setCookie);
+});
